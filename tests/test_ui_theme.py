d327b70144663362e0e922cf9f3d7957d7cdf84a
TerraLayer <321948909+terralayer@users.noreply.github.com>
2026-09-15 12:27:56 -0700

    Expect icon-only ScarletX header branding

--- tests/test_ui_theme.py
+++ tests/test_ui_theme.py
@@ -22,12 +22,13 @@ def test_scarlet_dark_design_tokens_are_the_default_theme():
     assert "--line:#252b34" in page
 
 
-def test_header_uses_approved_ribbon_wordmark_and_app_icon():
+def test_header_uses_approved_app_icon_without_text_wordmark():
     page = html()
     assert 'class="brandmark"' not in page
     assert '<span class="xslash xslash-a"></span>' not in page
     assert '<span class="xslash xslash-b"></span>' not in page
-    assert '<img src="/scarletx-wordmark.svg" alt="ScarletX">' in page
+    assert '<img src="/scarletx-wordmark.svg" alt="ScarletX">' not in page
+    assert '<div class="header-brand"><img src="/scarletx-icon.svg" alt="ScarletX"' in page
     assert '<link rel="icon" href="/scarletx-icon.svg" type="image/svg+xml">' in page
 
 
